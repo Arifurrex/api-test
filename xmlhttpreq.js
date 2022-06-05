e376a288b4b6c3446b255aec76937777d7de6a81
arifur rahman <arifurrex@gmail.com>
2022-06-05 17:16:23 +0600

    this is Api post

--- xmlhttpreq.js
+++ xmlhttpreq.js
@@ -1,25 +1,40 @@
 var btn= document.getElementById('btn');
 var details = document.getElementById('details');
-var idnumber = 1;
 console.log(btn);
 btn.addEventListener('click', function() {
-    const xml = () => {
+    const xml = (method,url,pdata) => {
         const xtp = new XMLHttpRequest();
-        xtp.open("GET", "https://jsonplaceholder.typicode.com/todos/"+idnumber);
+        xtp.open(method, url);
+        xtp.setRequestHeader('Content-type','application/json');
         xtp.onload = () => {
             const data = JSON.parse(xtp.responseText);
             htmlRender(data);
             console.log(data);
         };
-        xtp.send();
+        xml.onerror = () => {
+            console.log('error is here');
+        }
+        xtp.send(JSON.stringify(pdata));
     }
 
     const htmlRender = (data) => {
         details.insertAdjacentHTML('beforeend', `<h2 style="color:green;">
         title:${data.title}</h2>`);
     };
-    idnumber++
-    xml();
+
+    const getData = () => {
+        xml("GET", "https://jsonplaceholder.typicode.com/todos/1");
+    };
+    const postData = () => {
+        xml("POST", "https://jsonplaceholder.typicode.com/posts", {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+         });
+    };
+    postData();
+    // getData();
+    
 });
 
 
